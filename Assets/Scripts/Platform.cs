d372da72a0646b16e847a86691c63b26febccc4c
EleonoraTiritiello <ele99.249@gmail.com>
2021-02-26 12:30:49 +0100

    creare una mappa random di gameplay

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -3,18 +3,29 @@
 public class Platform : MonoBehaviour
 {
     public float jumpForce = 10f;
+    Player player;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D rb2D = collision.collider.GetComponent<Rigidbody2D>();
-
-        if(rb2D != null)
+        if(collision.relativeVelocity.y <= 0)
         {
-            Vector2 velocity = rb2D.velocity;
-            velocity.y = jumpForce;
-            rb2D.velocity = velocity;
+            Rigidbody2D rb2D = collision.collider.GetComponent<Rigidbody2D>();
+
+            if(rb2D != null)
+            {
+                Vector2 velocity = rb2D.velocity;
+                velocity.y = jumpForce;
+                rb2D.velocity = velocity;
+            }
+        }
+
+        if (player)
+        {
+            Destroy(gameObject);
         }
     }
+
+
 
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,39 +1,32 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private LayerMask platformsLayerMask;
-    private Rigidbody2D rb2D;
-    private BoxCollider2D boxCollider2D;
+    Rigidbody2D rb2D;
 
+    float movement = 0f;
+
+    public float movementSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
     }
 
-    //if they are on the ground and taken space, the player performs the jump
-    public void JumpPlayer()
+    private void FixedUpdate()
     {
-        if(isGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            float jumpVelocity = 100f;
-            rb2D.velocity = Vector2.up * jumpVelocity;
-        }
+        Vector2 velocity = rb2D.velocity;
+        velocity.x = movement;
+        rb2D.velocity = velocity;
+
     }
 
-
-    private bool isGrounded()
-    {
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down * 1f, platformsLayerMask);
-        return raycastHit2D.collider != null;
-    }
 }
