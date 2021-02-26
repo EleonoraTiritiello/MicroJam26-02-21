18ef40b068bfd33a118088a45993dbcea9e08383
EleonoraTiritiello <ele99.249@gmail.com>
2021-02-26 12:30:49 +0100

    creare una mappa random di gameplay

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformP;
+
+    public int numberofPlatform;
+    public float levelWidth = 3f;
+    public float minY = 2f;
+    public float maxY = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for(int i = 0; i < numberofPlatform; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x += Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformP, spawnPosition, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
