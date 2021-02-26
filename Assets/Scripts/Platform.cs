494813410672fc48bc1c678510ec7d9da42b6847
EleonoraTiritiello <ele99.249@gmail.com>
2021-02-26 12:00:38 +0100

    il player può saltare e passare da una piattaforma all'altra

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb2D = collision.collider.GetComponent<Rigidbody2D>();
+
+        if(rb2D != null)
+        {
+            Vector2 velocity = rb2D.velocity;
+            velocity.y = jumpForce;
+            rb2D.velocity = velocity;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformsLayerMask;
+    private Rigidbody2D rb2D;
+    private BoxCollider2D boxCollider2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //if they are on the ground and taken space, the player performs the jump
+    public void JumpPlayer()
+    {
+        if(isGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 100f;
+            rb2D.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down * 1f, platformsLayerMask);
+        return raycastHit2D.collider != null;
+    }
+}
